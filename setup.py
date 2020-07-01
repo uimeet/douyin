1bd0e513af891f64771debfb6adcfb7d588f6f4f
田真 <uimeet@gmail.com>
2020-07-01 22:52:48 +0800

    * 修改安装库路径

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@
 
 from setuptools import setup, find_packages  # noqa: H301
 
-NAME = "douyin/open/devtool/monitor"
+NAME = "douyin"
 VERSION = "1.0.0"
 # To install the library, run the following
 #
